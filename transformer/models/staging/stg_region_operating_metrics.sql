547d39098e3e86be9b8f5a3a926cdac13f08d6b7
Lily Kiziriya <107429746+LilyKizir@users.noreply.github.com>
2026-09-04 03:57:56 +0000

    typo fixed

--- transformer/models/staging/stg_region_operating_metrics.sql
+++ transformer/models/staging/stg_region_operating_metrics.sql
@@ -1,5 +1,5 @@
 with source as (
-        select * from {{ source('eia_energy', 'raw_region_operating_metrics') }}
+        select * from {{ source('eia_energy', 'raw_regional_operating_metrics') }}
   ),
   renamed as (
     select
