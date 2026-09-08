621862825fa58e52cfc9f613932b7845fb4fac52
Lily Kiziriya <107429746+LilyKizir@users.noreply.github.com>
2026-09-08 15:17:40 +0000

    adding basic sql statements to view tables

---
+++ scratch.sql
@@ -0,0 +1,11 @@
+select *
+from aea_lk_raw.raw_balancing_authority_interchange;
+
+select *
+from aea_lk_stage.stg_balancing_authority_interchange;
+
+select *
+from aea_lk_intermediate.fact_balancing_authority_interchange;
+
+select *
+from aea_lk_mart.mart_grid_interchange_summary;
